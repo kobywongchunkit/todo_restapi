c9cd5d7bd8be305435a1740c76cc34beee9bc5c6
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-16 14:28:18 +0800

    [todoAPI][koby] test:should_return_todos_when_perform_get_given_todos

---
+++ src/test/java/com/afs/restapi/controller/TodoControllerTest.java
@@ -0,0 +1,47 @@
+package com.afs.restapi.controller;
+
+import com.afs.restapi.entity.Todo;
+import com.afs.restapi.repository.TodoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoControllerTest {
+    @Autowired
+    TodoRepository todoRepository;
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void clearRepository(){
+        todoRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_todos_when_perform_get_given_todos() throws Exception {
+        //given
+        Todo todo = new Todo("1","todo text1",false);
+        todoRepository.insert(todo);
+        todo = new Todo("2","todo text2",true);
+        todoRepository.insert(todo);
+        //When
+        //then
+        mockMvc.perform(get("/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(2)))
+                .andExpect(jsonPath("$[0].text").value("todo text1"))
+                .andExpect(jsonPath("$[0].done").value(false))
+        ;
+
+    }
+}
