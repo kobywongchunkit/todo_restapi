4997d0caa0e26d1b3776767abc88805037682384
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-16 13:55:42 +0800

    [todoAPI][koby] Add TodoRequest and TodoResponse in dto

---
+++ src/main/java/com/afs/restapi/dto/TodoResponse.java
@@ -0,0 +1,34 @@
+package com.afs.restapi.dto;
+
+public class TodoResponse {
+    private String id;
+    private String text;
+    private Boolean done;
+
+    public TodoResponse() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
