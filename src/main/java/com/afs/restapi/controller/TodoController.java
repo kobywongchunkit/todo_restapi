d8ffd07851afa89fac6603848a32f94b6d07e98d
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-16 14:12:34 +0800

    [todoAPI][koby] feat: Add TodoController to get all todo

---
+++ src/main/java/com/afs/restapi/controller/TodoController.java
@@ -0,0 +1,24 @@
+package com.afs.restapi.controller;
+
+import com.afs.restapi.entity.Todo;
+import com.afs.restapi.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos(){
+        return todoService.findAll();
+    }
+}
