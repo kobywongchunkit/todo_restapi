38572c86b5e26680912cfb02ad3ffc618480815e
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-16 14:05:01 +0800

    [todoAPI][koby] feat: Add TodoRepository with MongoDB setup

---
+++ src/main/java/com/afs/restapi/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.afs.restapi.repository;
+
+import com.afs.restapi.entity.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo,String> {
+}
